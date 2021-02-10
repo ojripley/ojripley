e405b55233edb675ce76d703c41a3d2fd39abd3a
ojripley <ojripley19@gmail.com>
2021-02-10 18:26:02 -0500

    cleanup

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,11 +12,7 @@ export default function Bio() {
       {/* <hr className='bio-divide'></hr> */}
       <div className='bio-text-section'>
         <p className='bio-header'>Who am I?</p>
-        {/* <p className='bio-text'>&nbsp;&nbsp;&nbsp;&nbsp; Software Developer. &nbsp;&nbsp;&nbsp;&nbsp; Runner. &nbsp;&nbsp;&nbsp;&nbsp; Presser of Buttons. &nbsp;&nbsp;&nbsp;&nbsp; Freshwater Surfer. &nbsp;&nbsp;&nbsp;&nbsp;</p> */}
         <p className='bio-text'>Software Developer. Distance Runner. Sailor, Surfer. Presser of Buttons.</p>
-        {/* <p className='bio-text'>Distance Runner.</p>
-        <p className='bio-text'>Sailor, Surfer.</p>
-        <p className='bio-text'>Presser of Buttons.</p>*/}
         <p className='bio-header'>Okay, but seriously...</p>
         <p className='bio-text'>I am a fullstack software developer, based out of <span className='bio-text-emphasized'>Toronto, Ontario</span>. I like discovering the unknown, spending time outdoors, reading books and having enough phone battery to roll to the other side of the bed.</p>
         <p className='bio-header'>What can I do?</p>
